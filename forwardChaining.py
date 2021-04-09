2bfeca26f104d84335c5659365774d2d0babff65
johyeon yoon <yds03080@naver.com>
2021-04-09 16:04:49 +0900

    Refactor forwardChaining.py

--- forwardChaining.py
+++ forwardChaining.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 file=open(input('file:'))
 line=file.readlines()
@@ -29,6 +30,7 @@ for i in range(len(Rules)):
         print(i,' ',end='')
     print('->',rule[0])
     R.append([rule[1:],rule[0]])
+
 print()
 print(' ',end='')
 print('2)Facts')
@@ -44,7 +46,8 @@ print(Goal);print()
 # ///////////////////////////////////////
 Path=[]
 Flag=['*']
-fact=[]
+Flag = deque(Flag)
+fact = Fact
 for i in Fact:
     fact.append(i)
 print('PART2. Trace')
@@ -58,52 +61,53 @@ while Goal not in Fact and Yes==False: #fact에 최종 원소가 추가되거나
     print('ITERATION',count)
     K=-1
     apply=False
-    while K<=len(R)-2 and apply==False: #하나의 적용되는 규칙을 발견할 때까지
+    while K<len(R)-1 and apply==False: #하나의 적용되는 규칙을 발견할 때까지
+        FlagYes = False
         K=K+1
         print('     ', end='')
         print('R', K + 1, ':', end='')
         for i in R[K][0]:
             print(i, '', end='')
         print('->',R[K][-1], end='')
-        FlagYes = False
-        if FlagYes==False:
-            if str(K+1) in Flag: #깃발 있는 경우
-                # FlagYes=True
-                a=0
-                for t in Flag: #인덱스 찾기
-                    if t!=str(K+1):
-                        a=a+1
-                    else:
-                        b=a+1
-                if Flag[b]==[1]:
-                    print(', skip, because flag1 raised')
-                elif Flag[b]==[2]:
-                    print(', skip, because flag2 raised')
 
-            else: #깃발 없다.
-                lbool=False
-                for i in R[K][0]:
-                    for j in i:
-                        if lbool==False:
-                            if j in Fact:
-                                if R[K][1] in Fact:  # 우변이 이미 있는 경우
-                                    Flag.append(str(K+1));Flag.append([2])
-                                    print(' not applied, because RHS in facts. Raise flag2')
-                                    FlagYes = True
-                                    apply=False
-                                else:
-                                    apply = True
-                                    P=K+1
-                            else:
+        if str(K+1) in Flag: #깃발 있는 경우
+            a=0
+            for t in Flag: #인덱스 찾기
+                if t!=str(K+1):
+                    a += 1
+                else:
+                    b=a+1
+                    break
+            if Flag[b]==[1]:
+                print(', skip, because flag1 raised')
+            elif Flag[b]==[2]:
+                print(', skip, because flag2 raised')
+
+        else: #깃발 없다.
+            lbool=False
+            for i in R[K][0]:
+                for j in i:
+                    if lbool==False:
+                        if j in Fact:
+                            if R[K][1] in Fact:  # 우변이 이미 있는 경우
+                                Flag.append(str(K+1));Flag.append([2])
+                                print(' not applied, because RHS in facts. Raise flag2')
+                                FlagYes = True
                                 apply=False
-                                print(', not applied, because of lacking ', j)
-                                lbool=True
+                            else:
+                                apply = True
+                                P=K+1
+                                break
+                        else:
+                            apply=False
+                            print(', not applied, because of lacking ', j)
+                            lbool=True
         if apply==True:
             Fact.append(R[P-1][-1])
             Flag.append(str(P));Flag.append([1])
             Path.append(P)
             if '*' in Flag:
-                del (Flag[0])
+                Flag.popleft()
             print(', apply, Raise flag1. Facts',' ', end='')
             for i in Fact:
                 print(i,' ', end='')
